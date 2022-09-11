c39ab75b3afc156ecb87c271388338d4c90edbb5
dokesz <dokesz.uzleti@gmail.com>
2022-09-11 15:42:02 +0200

    preprocessor example

---
+++ preprocessor.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+//preprocesszorral definiálj egy alma nevű integer konstanst, majd add értékül egy almafa változódnak
+#define alma 5
+
+int main(){
+    int almafa = alma;
+    std::cout << almafa;
+    return 0;
+}
