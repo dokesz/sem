0325df151bdce546d317b84d9cacb079c03c8e7f
dokesz <dokesz.uzleti@gmail.com>
2022-09-21 12:14:31 +0200

    small changes

--- szotarprojekt/func.hpp
+++ szotarprojekt/func.hpp
@@ -1,1 +1,6 @@
 #pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
--- szotarprojekt/szotar.cpp
+++ szotarprojekt/szotar.cpp
@@ -1,7 +1,4 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-using namespace std;
+#include <func.hpp>
 
 //#define MAX 10
 
