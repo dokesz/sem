204e513c1e4d2401ac55f8156baa7303e766347e
dokesz <dokesz.uzleti@gmail.com>
2022-09-21 12:11:36 +0200

    reading-fix

--- szotarprojekt/szotar.cpp
+++ szotarprojekt/szotar.cpp
@@ -3,18 +3,41 @@
 #include <string>
 using namespace std;
 
-int main(){
-    string* text=new string;
+//#define MAX 10
+
+int main()
+{
     ifstream reading("szotar.txt");
-    if(reading.is_open()){
-        for(int i=0; i != reading.eof(); i++){
-            getline(reading, text[i]);
+    int db = 0;
+    string text;
+    // string* text = new string[MAX];
+    if (reading.is_open())
+    {
+        while (!reading.eof())
+        {
+            getline(reading, text);
+            db++;
         }
-    reading.close();
-    for (int i = 0; i < text->size(); i++){
-        cout << text[i];
+
+        reading.close();
+
+        ifstream reading("szotar.txt");
+        string *sorok = new string[db];
+        for (int i = 0; i < db; i++)
+        {
+            getline(reading, sorok[i]);
+            // cout << "Beolvasas" ;
+        }
+
+        reading.close();
+
+        for (int i = 0; i < db; i++)
+        {
+            cout << sorok[i] << endl;
+        }
     }
-    }else
+    else
         cout << "sikertelen nyitas";
+    // cout << db;
     return 0;
 }
